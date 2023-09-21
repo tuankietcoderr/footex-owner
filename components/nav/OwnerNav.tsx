b253238b8330655c27b45e2e5476f44bb8b859bf
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-09-21 09:32:39 +0700

    Fix bug

--- components/nav/OwnerNav.tsx
+++ components/nav/OwnerNav.tsx
@@ -7,7 +7,7 @@ import { Input } from "../ui/input"
 
 const OwnerNav = () => {
   return (
-    <div className="flex items-center justify-between gap-2 rounded-lg border border-border bg-card px-4 py-2">
+    <div className="flex items-center justify-between gap-2 rounded-lg border border-border bg-card px-4 py-2 shadow-sm">
       <div className="flex items-center gap-2">
         <Search />
       </div>
