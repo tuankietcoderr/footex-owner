859f20ef73083150fc7c5904a43246a2495af655
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-12-29 20:25:26 +0700

    fix bug

--- app/chi-nhanh/page.tsx
+++ app/chi-nhanh/page.tsx
@@ -1,11 +1,14 @@
 import BranchSwitcher from "@/components/nav/branch-switcher"
+import Base from "./_components/base"
 
 const page = async () => {
   return (
     <div className="flex min-h-screen items-center justify-center overflow-auto">
-      <div>
-        <BranchSwitcher branchId="" />
-      </div>
+      <Base>
+        <div>
+          <BranchSwitcher branchId="" />
+        </div>
+      </Base>
     </div>
   )
 }
